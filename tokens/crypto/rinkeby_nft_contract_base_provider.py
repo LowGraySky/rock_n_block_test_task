a6ff67f55488cfe2683f6bef3a623541e8518ee3
LowGraySky <nick.alexeevkz@gmail.com>
2022-04-05 00:37:02 +0300

    remove redurant methods from nft contract provider

--- tokens/crypto/rinkeby_nft_contract_base_provider.py
+++ tokens/crypto/rinkeby_nft_contract_base_provider.py
@@ -11,10 +11,3 @@ class RinkebyNFTContractBaseProvider(metaclass=ABCMeta):
     def totalSupply(self):
         raise NotImplementedError("Not implemented 'totalSupply' method")
 
-    @abstractmethod
-    def signTransaction(self, transaction, private_key):
-        raise NotImplementedError("Not implemented 'signTransaction' method")
-
-    @abstractmethod
-    def sendTransaction(self, signed_transaction):
-        raise NotImplementedError("Not implemented 'sendTransaction' method")
