a32216e4924620c56a939f88ff07c69c56ab300e
LowGraySky <nick.alexeevkz@gmail.com>
2022-04-08 00:25:19 +0300

    remove redurant params in db_column in model class

--- tokens/models.py
+++ tokens/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Token(models.Model):
     id = models.BigAutoField(primary_key=True)
-    unique_hash = models.CharField(max_length=20, db_column='tokens.unique_hash')
-    tx_hash = models.CharField(max_length=100, blank=True, db_column='tokens.tx_hash')
-    media_url = models.URLField(db_column='tokens.media_url')
-    owner = models.CharField(max_length=42, db_column='tokens.owner')
+    unique_hash = models.CharField(max_length=20, db_column='unique_hash')
+    tx_hash = models.CharField(max_length=100, blank=True, db_column='tx_hash')
+    media_url = models.URLField(db_column='media_url')
+    owner = models.CharField(max_length=42, db_column='owner')
 
     @classmethod
     def create(cls, unique_hash, tx_hash, media_url, owner):
