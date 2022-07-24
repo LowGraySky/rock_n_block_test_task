2816b8a042db91c353e1a28ba8fdf273aaf489c0
nalekseev <nalekseev@staff.emphasoft.com>
2022-07-25 00:56:16 +0300

    Remake model and add serializer

--- tokens/models.py
+++ tokens/models.py
@@ -2,21 +2,11 @@ from django.db import models
 
 
 class Token(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    unique_hash = models.CharField(max_length=20, db_column='unique_hash')
-    tx_hash = models.CharField(max_length=200, blank=True, db_column='tx_hash')
-    media_url = models.URLField(db_column='media_url')
-    owner = models.CharField(max_length=42, db_column='owner')
-
-    @classmethod
-    def create(cls, unique_hash, tx_hash, media_url, owner):
-        token = cls(
-            unique_hash=unique_hash,
-            tx_hash=tx_hash,
-            media_url=media_url,
-            owner=owner
-        )
-        return token
+    unique_hash = models.CharField(max_length=20, unique=True)
+    tx_hash = models.CharField(max_length=200)
+    media_url = models.URLField()
+    owner = models.CharField(max_length=42)
+    send_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"unique_hash: {self.unique_hash} tx_hash: {self.tx_hash} media_url: {self.media_url} owner: {self.owner}"
+        return f"id {self.id} unique_hash: {self.unique_hash} tx_hash: {self.tx_hash} media_url: {self.media_url} owner:{self.owner} send_at: {self.send_at}"
---
+++ tokens/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from tokens.models import Token
+
+
+class CreateTokenQueryParamsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Token
+        fields = ["media_url ", "owner "]
