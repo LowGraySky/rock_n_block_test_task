5b1fdee396c9701c141c3402708f9bbbf1b458ad
nalekseev <nalekseev@staff.emphasoft.com>
2022-07-25 02:40:30 +0300

    Change model class

--- tokens/models.py
+++ tokens/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Token(models.Model):
     unique_hash = models.CharField(max_length=20, unique=True)
-    tx_hash = models.CharField(max_length=200)
+    tx_hash = models.CharField(max_length=200, blank=True)
     media_url = models.URLField()
     owner = models.CharField(max_length=42)
     send_at = models.DateTimeField(auto_now=True)
