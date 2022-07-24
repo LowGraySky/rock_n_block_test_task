256796ed481381082180b6bbe8108099bc5d4c92
nalekseev <nalekseev@staff.emphasoft.com>
2022-07-25 02:41:11 +0300

    Remove redurant space

--- tokens/serializers.py
+++ tokens/serializers.py
@@ -4,7 +4,6 @@ from tokens.models import Token
 
 
 class CreateTokenQueryParamsSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Token
         fields = ["media_url ", "owner "]
