d8d80a837458aee36621e2d831c8a6d868afb769
nalekseev <nalekseev@staff.emphasoft.com>
2022-07-25 05:49:26 +0300

    Add tokens serializer

--- tokens/serializers.py
+++ tokens/serializers.py
@@ -3,7 +3,12 @@ from rest_framework import serializers
 from tokens.models import Token
 
 
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = "__all__"
+
+
 class CreateTokenQueryParamsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
-        fields = ["media_url ", "owner "]
+        fields = ["media_url", "owner"]
